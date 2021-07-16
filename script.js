f4f5188cb5c8101a22388f0c8a74c23fc6f665e4
noaha27 <67342736+noaha27@users.noreply.github.com>
2021-07-16 20:48:03 +0000

    f

--- script.js
+++ script.js
@@ -6,13 +6,43 @@ let canvas;
 let textDiv;
 let textP;
 let buttonDiv;
-let trainButton;
+let trainButton; 
+let radioDiv;
+let notesRadio;
+
 
 // Global ML Variables
-//let model;
+let model;
+let state;
+let env;
+let wave;
+let notes;
 
 function setup() {
-
+canvasDiv = createDiv();
+canvas = createCanvas(640, 480);
+canvas.parent(canvasDiv);
+canvas.mousePressed(canvasClicked);
+textDic = createDiv();
+textP = createP("Step 1: Data Collection");
+textP.parent(textDiv);
+buildButtons();
+state = "collection";
+notes = {
+  C: 261.6256,
+  D: 293.6648,
+  E: 329.6276,
+  F: 349.2282,
+  G: 391.9954
+  };
+  let options = {
+    inputs: ["x", "y"],
+    outputs: ["label"],
+    task: "classification",
+    debug: true
+  };
+  model = ml5.neuralNetwork(options);
+  createMusicSystem(); 
 }
 
 function draw() {
@@ -20,7 +50,19 @@ function draw() {
 }
 
 function buildButtons() {
-
+  radioDiv = createDiv();
+  notesRadio = createRadio();
+  notesRadio.option("C");
+  notesRadio.option("D");
+  notesRadio.option("E");
+  notesRadio.option("F");
+  notesRadio.option("G");
+  notesRadio.selected("C");
+  notesRadio.parent(radioDiv);
+  buttonDiv = createDiv();
+  trainButton = createButton("Train Model");
+  trainButton.parent(buttonDiv);
+  trainButton.mousePressed(trainModel);
 }
 
 function createMusicSystem() {
@@ -39,21 +81,53 @@ function trainModel() {
 }
 
 function whileTraining(epoch, loss) {
-
+  console.log(epoch);
 }
 
 function finishedTraining() {
-
+  state = "prediction";
+  textP.html("step 3: Prediction");
 }
 
 function drawNote(note, noteColor, ellipseColor) {
+  stroke(0);
+  fill(ellipseColor);
+  ellipse(mouseX, mouseY, 24);
+  fill(noteColor);
+  noStroke();
+  textAlign();
+  textAlign(CENTER, CENTER);
+  text(note, mouseX, mouseY)
 
 }
 
 function canvasClicked() {
-
+  let inputs = {
+    x: mouseX,
+    y: mouseY
+  };
+  //handle the various outcomes, depending on program state
+  if(state === "collection") {
+    let targetLabel = notesRadio.value();
+    let target = {
+      label: targetLabel
+    };
+    model.addData(inputs, target);
+    drawNote(targetLabel, "black", "white");
+    wave.freq(notes[targetLabel]);
+    env.play();
+  } else if(state === "prediction") {
+    model.classify(inputs, gotResults);
+  }
 }
 
 function gotResults(error, results) {
-
+   if(error) {
+     console.error(error);
+   } else {
+     let label = results[0].label;
+     drawNote(label, "white", "blue");
+     wave.freq(notes[label]);
+     env.play();
+   }
 }
